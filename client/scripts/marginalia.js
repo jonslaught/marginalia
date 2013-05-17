81a70e161e346c65b1f5053584600c13bee911fa
Yuhki Yamashita <yuhki@outlook.com>
2013-05-16 23:10:07 -0700

    Updated the highlight algorithm to be more generic. Also did some minor tweaks like bringing the titles in and touching up the highlight CSS.

--- client/scripts/marginalia.js
+++ client/scripts/marginalia.js
@@ -1,20 +1,34 @@
-function assignColor (frequency) {
-  var className = "noFreq";
+function NTile (array, n) {
+  var self = this;
+  this.boundaries = [];
   
-  // TODO: replace with generic algorithm
-  if (frequency > 10000) {
-    className = "highFreq"; 
-  } else if (frequency > 7000) {
-    className = "highMidFreq";
-  } else if (frequency > 5000) {
-    className = "midFreq";
-  } else if (frequency > 3000) {
-    className = "lowMidFreq";
-  } else if (frequency > 1000) {
-    className = "lowFreq";
+  // "constructor" -- TODO: clean up, since it's by no means foolproof
+  var numbers = [];
+  for (var key in array) {
+    numbers.push(array[key]);
+  }
+    
+  // sort numerically
+  numbers.sort(function(a,b){return a - b});
+  var count = numbers.length;
+  
+  if (n > 0 && n <= count) {
+    for (var i = 0; i < n; i++) {
+      self.boundaries.push(numbers[Math.floor(i * count / n)]);
+    }
   }
 
-  return className;
+  this.assignColor = function (frequency) {
+    var className = "freq";
+    for (var i = 0; i < self.boundaries.length; i++) {
+      if (frequency <= self.boundaries[i]) {
+        return className + i;
+      }
+    }
+    
+    return className + self.boundaries.length;
+  };
+
 }
 
 
@@ -22,25 +36,25 @@ SOURCE = $.url().param('source');
 VERSION = 'shortones'
 
 $(document).ready(function() {
-
-
   // Load the HTML file
   $.get('../data/' + VERSION + '_' + SOURCE + '_marked.html',function(html){
     $('.article .text').html(html);
+    $('#articleTitle').html($('#textTitle').html());
 
     // Load the counts data
     $.getJSON('../data/' + VERSION + '_' + SOURCE + '_counts.js',function(metadata) {
 
-    // Do the marking
+      // Calculate n-tile (5 for now)
+      nTile = new NTile(metadata, 5);
+
+      // Do the marking
       $(".marker").each(function(index) {
         var id = $(this).attr("id");
         var frequency = metadata[id];
-        console.log([id,frequency]);
         if (frequency) {
-          $(this).addClass(assignColor(frequency));
+          $(this).addClass(nTile.assignColor(frequency));
         }
       });
-
     });
   });
 });
